f681135abc5d980009c3c2177109885124b9c57c
horikawa <horikawa@bemax.jp>
2016-09-01 09:36:14 +0900

    問題を3つ作成

---
+++ lesson1-3.rb
@@ -0,0 +1,24 @@
+# 次の配列を基本選択法で昇順に整列せよ
+
+ary = [34,29,783,73,12,238]
+
+i =
+while      do
+	min =
+	j = 
+	while     do
+		if         then
+			min =
+		end
+	j = 
+	end
+	
+	w = ary[]
+	ary[] = ary[]
+	ary[] = w
+	
+	i = 
+end
+
+puts ary
+
